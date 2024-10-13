e34869f082d425b9d34af9e7e8f8eea21890c076
frumpfruit <fernandabekasi@gmail.com>
2024-10-13 15:01:23 +0700

    Building football cards step 40

--- script.js
+++ script.js
@@ -181,16 +181,27 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {
-      `
+  playerCards.innerHTML += arr
+    .map(
+      ({ name, position, number, isCaptain, nickname }) =>
+        `
         <div class="player-card">
           <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
           <p>Position: ${position}</p>
           <p>Number: ${number}</p>
-          <p>Nickname: </p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
-      `;
-    }
-  );
+      `
+    )
+    .join("");
 };
+
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+  }
+});
